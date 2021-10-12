8dacefd9498f84f35607297340e71142e9be3987
Arlo_The_Mini <83123367+andywoodruff6@users.noreply.github.com>
2021-10-12 12:17:27 -0400

    init MyToken test working

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,31 +1,16 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// When running the script with `npx hardhat run <script>` you'll find the Hardhat
-// Runtime Environment's members available in the global scope.
 const hre = require("hardhat");
 // const startTime  = 13404000;
 // const endTime    = 13404500;
 
 async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
+  const Token = await hre.ethers.getContractFactory("MyToken");
+  const token = await Token.deploy();
 
-  // We get the contract to deploy
-  const Greeter = await hre.ethers.getContractFactory("MyToken");
-  const greeter = await Greeter.deploy();
+  await token.deployed();
 
-  await greeter.deployed();
-
-  console.log("MyToken deployed to:", greeter.address);
+  console.log("MyToken deployed to:", token.address);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main()
   .then(() => process.exit(0))
   .catch((error) => {
--- test/MyToken-test.js
+++ test/MyToken-test.js
@@ -1,13 +1,24 @@
 const { expect } = require("chai");
-const hre, { ethers } = require("hardhat");
+const { ethers } = require("hardhat");
 
 
-describe('MyToken', () => {
-    let MyToken;
-
-    beforeEach(async function () {
-        const contractName = 'MyToken';
-        await hre.run("compile");
-
+describe('MyToken contract', function () {
+    it("Deployment should assign the total supply to the owner", async function() {
+        const [owner] = await ethers.getSigners();
+        const Token = await ethers.getContractFactory('MyToken');
+        const hhToken = await Token.deploy();
+        const ownerBalance = await hhToken.balanceOf(owner.address);
+        expect(await hhToken.totalSupply()).to.equal(ownerBalance);
     })
 });
+
+ // let token
+ // beforeEach( async () => {
+ //     token = await Token.new()
+ // }) // by calling async await here, we do not need to write these two lines for every "it" test
+ // describe('deployment', () => {
+ //     it('assigns the total supply to the depoloyer', async () => {
+ //         const result = await token.balanceOf(deployer)
+ //         result.toString().should.equal(totalSupply.toString())
+ //     }) 
+ // })
