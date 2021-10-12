cd79a65d9079f653791c7a0fbe5699a4ecc98de0
Arlo_The_Mini <83123367+andywoodruff6@users.noreply.github.com>
2021-10-12 08:47:25 -0400

    filled in MyToken

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -1,6 +1,12 @@
 //SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
+/**
+@title MyToken
+@author Andy W
+@notice A token with a time window for transferring
+*/
+import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 // tokens can only be transferred after _startTime but before _endTime - provided in constructor
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,9 @@
 require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
+
+const { task } = require("hardhat/config");
+const ethers = require('ethers');
+
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -10,6 +15,8 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   }
 });
 
+task("tokenBalance", "Prints the balance of an account", async ())
+
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
@@ -18,4 +25,11 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: "0.8.4",
+  // etherscan: {
+  //   apiKey:
+  // },
+  local: {
+    url: 'http://127.0.0.1:8545/',
+    accounts: [.env.PRIVATE_KEY]
+  }
 };
