8d0e1d448dd5cb134056455e77855ac383930d17
Arlo_The_Mini <83123367+andywoodruff6@users.noreply.github.com>
2021-10-12 12:38:13 -0400

    tests for basic functionality

--- test/MyToken-test.js
+++ test/MyToken-test.js
@@ -1,24 +1,79 @@
 const { expect } = require("chai");
-const { ethers } = require("hardhat");
 
+describe("Token contract", function () {
 
-describe('MyToken contract', function () {
-    it("Deployment should assign the total supply to the owner", async function() {
-        const [owner] = await ethers.getSigners();
-        const Token = await ethers.getContractFactory('MyToken');
-        const hhToken = await Token.deploy();
-        const ownerBalance = await hhToken.balanceOf(owner.address);
-        expect(await hhToken.totalSupply()).to.equal(ownerBalance);
-    })
+  let Token;
+  let hardhatToken;
+  let owner;
+  let addr1;
+  let addr2;
+  let addrs;
+
+  // `beforeEach` will run before each test, re-deploying the contract every time.
+  beforeEach(async function () {
+
+    Token = await ethers.getContractFactory("MyToken");
+    [owner, addr1, addr2, ...addrs] = await ethers.getSigners();
+
+    hardhatToken = await Token.deploy();
+  });
+
+  describe("Deployment", function () {
+    // it("Should set the right owner", async function () {
+    //   expect(await hardhatToken.owner()).to.equal(owner.address);
+    // });
+
+    it("Should assign the total supply of tokens to the owner", async function () {
+      const ownerBalance = await hardhatToken.balanceOf(owner.address);
+      expect(await hardhatToken.totalSupply()).to.equal(ownerBalance);
+    });
+  });
+
+  describe("Transactions", function () {
+    it("Should transfer tokens between accounts", async function () {
+      // Transfer 50 tokens from owner to addr1
+      await hardhatToken.transfer(addr1.address, 50);
+      const addr1Balance = await hardhatToken.balanceOf(addr1.address);
+      expect(addr1Balance).to.equal(50);
+
+      // Transfer 50 tokens from addr1 to addr2
+      // Use .connect(signer) to send a transaction from another account
+      await hardhatToken.connect(addr1).transfer(addr2.address, 50);
+      const addr2Balance = await hardhatToken.balanceOf(addr2.address);
+      expect(addr2Balance).to.equal(50);
+    });
+
+    it("Should fail if sender doesn’t have enough tokens", async function () {
+      const initialOwnerBalance = await hardhatToken.balanceOf(owner.address);
+
+      await expect(
+        hardhatToken.connect(addr1).transfer(owner.address, 1)
+      ).to.be.revertedWith("ERC20: transfer amount exceeds balance");
+
+      // Owner balance shouldn't have changed.
+      expect(await hardhatToken.balanceOf(owner.address)).to.equal(
+        initialOwnerBalance
+      );
+    });
+
+    it("Should update balances after transfers", async function () {
+      const initialOwnerBalance = await hardhatToken.balanceOf(owner.address);
+
+      // Transfer 100 tokens from owner to addr1.
+      await hardhatToken.transfer(addr1.address, 100);
+
+      // Transfer another 50 tokens from owner to addr2.
+      await hardhatToken.transfer(addr2.address, 50);
+
+      // Check balances.
+      const finalOwnerBalance = await hardhatToken.balanceOf(owner.address);
+      expect(finalOwnerBalance).to.equal(initialOwnerBalance - 150);
+
+      const addr1Balance = await hardhatToken.balanceOf(addr1.address);
+      expect(addr1Balance).to.equal(100);
+
+      const addr2Balance = await hardhatToken.balanceOf(addr2.address);
+      expect(addr2Balance).to.equal(50);
+    });
+  });
 });
-
- // let token
- // beforeEach( async () => {
- //     token = await Token.new()
- // }) // by calling async await here, we do not need to write these two lines for every "it" test
- // describe('deployment', () => {
- //     it('assigns the total supply to the depoloyer', async () => {
- //         const result = await token.balanceOf(deployer)
- //         result.toString().should.equal(totalSupply.toString())
- //     }) 
- // })
